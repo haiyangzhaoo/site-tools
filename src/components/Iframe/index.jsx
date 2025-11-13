5b97c83e852d45e2338fa9cb30919b07e487c29c
haiyang.zhao <1449235957@qq.com>
2025-11-13 17:16:30 +0800

    pc wev

--- src/components/Iframe/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import s from './index.module.scss'
-
-export default function Search(props) {
-    return <iframe className={s.iframe} src={props.url}></iframe>
-}
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,40 +1,33 @@
-import { Routes, Route, HashRouter } from "react-router-dom";
-import React, { useEffect } from "react";
-import titleMap from "../utils/titleMap";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const Home = React.lazy(() => import('../pages/Home'))
-const Demo = React.lazy(() => import('../pages/Demo'))
-const Like = React.lazy(() => import('../pages/Like'))
-const Iframe = React.lazy(() => import('../components/Iframe'))
-const Search = React.lazy(() => import('../pages/Search'))
+import { Layout } from "../layout/Layout";
+import { Home } from "../pages/home/Home";
+import { Channels } from "../pages/channels/Channels";
+import { About } from "../pages/about/About";
+
+export const routers = [
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/channels',
+        element: <Channels />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      }
+    ]
+  }
+];
 
 export default function Router() {
-    useEffect(() => {
-        document.title = titleMap[window.location.hash.substr(1)]
-        
-        const cb = e => {
-            document.title = titleMap[new URL(e.newURL).hash.substr(1)]
-        }
+  const router = createBrowserRouter(routers);
 
-        window.addEventListener('hashchange', cb)
-
-        return () => {
-            window.removeEventListener('hashchange', cb)
-        }
-    }, [])
-
-    return <HashRouter>
-        <Routes>
-            <Route exact path="/" element={<Search />} />
-            {/* <Route path="/ky" element={<Demo />} />
-            <Route path="/like" element={<Like />} errorElement={<div>error</div>}>
-                <Route path="search" element={<Iframe url='https://cn.bing.com/' />}></Route>
-                <Route path="network">
-                    <Route path="juejin" element={<Iframe url='https://juejin.cn/' />}></Route>
-                    <Route path="yuque" element={<Iframe url='https://www.yuque.com/dashboard' />}></Route>
-                    <Route path="bili" element={<Iframe url='https://bilibili.com' />}></Route>
-                </Route>
-            </Route> */}
-        </Routes>
-    </HashRouter>
+  return <RouterProvider router={router} />;
 }
