f2d50818e3580c4ab42339eaeb72040b7f8f13f2
haiyang.zhao <1449235957@qq.com>
2025-11-17 18:00:20 +0800

    update:iptv source 11/17/2025, 6:00:20 PM

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { Layout as AntdLayout } from 'antd';
 import { Outlet } from 'react-router-dom';
 
+const { Header, Content, Footer } = AntdLayout;
+
 export const Layout = () => {
-  return <>
-    <div>Layout</div>
-    <Outlet />
-  </>
+  return <AntdLayout>
+    <Header>header</Header>
+    <Content>
+      <Outlet />
+    </Content>
+    <Footer>footer</Footer>
+  </AntdLayout>
 }
